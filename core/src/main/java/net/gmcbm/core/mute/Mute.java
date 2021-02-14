0cd0c32c7b1f2fb4c72d387b6b5e47e78805bdf5
Ursin Filli <git@ursinn.dev>
2021-02-14 01:33:25 +0100

    Add Ban, Mute and Warn

---
+++ core/src/main/java/net/gmcbm/core/mute/Mute.java
@@ -0,0 +1,52 @@
+package net.gmcbm.core.mute;
+
+import net.gmcbm.core.item.Item;
+import net.gmcbm.core.item.ItemAction;
+import net.gmcbm.core.item.ItemStatus;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * @author Ursin Filli
+ * @version 1.0
+ * @since 1.0
+ */
+public class Mute extends Item {
+
+    /**
+     * Constructor of Mute Item
+     *
+     * @param id         Mute Identifier
+     * @param player     {@link UUID} of Player
+     * @param reason     Reason
+     * @param issuer     Issuer {@link UUID} or null
+     * @param issuerName Issuer Name
+     * @param from       From {@link Instant}
+     * @param until      Until {@link Instant}
+     * @param status     Status ({@link ItemStatus})
+     * @param action     Action ({@link ItemAction})
+     */
+    public Mute(@Nonnull String id, @Nonnull UUID player, @Nonnull String reason, @Nullable UUID issuer,
+                @Nonnull String issuerName, @Nonnull Instant from, @Nonnull Instant until,
+                @Nonnull ItemStatus status, @Nonnull ItemAction action) {
+        super(id, player, reason, issuer, issuerName, from, until, status, action);
+    }
+
+    /**
+     * Constructor of Mute Item
+     *
+     * @param player     {@link UUID} of Player
+     * @param reason     Reason
+     * @param issuer     Issuer {@link UUID} or null
+     * @param issuerName Issuer Name
+     * @param from       From {@link Instant}
+     * @param until      Until {@link Instant}
+     */
+    public Mute(@Nonnull UUID player, @Nonnull String reason, @Nullable UUID issuer, @Nonnull String issuerName,
+                @Nonnull Instant from, @Nonnull Instant until) {
+        super(player, reason, issuer, issuerName, from, until);
+    }
+}
