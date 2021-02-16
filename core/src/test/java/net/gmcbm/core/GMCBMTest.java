8ee729809a949754eae8fd2c4d8d7aa5c633264f
Ursin Filli <git@ursinn.dev>
2021-02-16 22:01:48 +0100

    Add Tests

---
+++ core/src/test/java/net/gmcbm/core/GMCBMTest.java
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 The GMCBM Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package net.gmcbm.core;
+
+import net.gmcbm.core.server.Server;
+import net.gmcbm.core.utils.PluginType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+/**
+ * @author Ursin Filli
+ * @version 1.0
+ * @since 1.0
+ */
+class GMCBMTest {
+
+    @Test
+    void testCoverage() {
+        GMCBM gmcbm = new GMCBM(PluginType.BUKKIT, false, new Server(UUID.fromString("3eb0f3b9-a260-4f57-b959-89ed6be64423")));
+
+        Assertions.assertEquals(PluginType.BUKKIT, gmcbm.getType(), "Coverage");
+        Assertions.assertFalse(gmcbm.isDebug(), "Coverage");
+        Assertions.assertEquals(gmcbm.getServer().getId(), UUID.fromString("3eb0f3b9-a260-4f57-b959-89ed6be64423"), "Coverage");
+    }
+}
