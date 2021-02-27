68aedd00c8e59f717a47e22bbc22c123d190901f
Ursin Filli <git@ursinn.dev>
2021-02-27 15:34:42 +0100

    Update GmcbmCommand

--- core/src/main/java/net/gmcbm/core/commands/GmcbmCommand.java
+++ core/src/main/java/net/gmcbm/core/commands/GmcbmCommand.java
@@ -58,6 +58,12 @@ public class GmcbmCommand extends BaseCommand {
         sender.sendMessage("Plugin Platform: " + gmcbm.getType().toString());
         sender.sendMessage("Plugin Version: " + gmcbm.getVersion());
         sender.sendMessage("Plugin Debug: " + gmcbm.isDebug());
-        sender.sendMessage("ServerId: " + gmcbm.getServer().getId().toString());
+        if (gmcbm.getServer().getId() == null) {
+            sender.sendMessage("Server Id: " + gmcbm.getServer().getId().toString());
+        }
+        if (gmcbm.getServer().getOwner() == null) {
+            sender.sendMessage("Owner Id: " + gmcbm.getServer().getOwner().getId().toString());
+            sender.sendMessage("Owner Name: " + gmcbm.getServer().getOwner().getName());
+        }
     }
 }
